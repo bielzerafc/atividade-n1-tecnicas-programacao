94a6ee45291ae32aa4213ea0a4d7cb31a4b6763e
bielzerafc <89857240+bielzerafc@users.noreply.github.com>
2021-09-07 13:04:01 -0300

    Update N1.py

--- N1.py
+++ N1.py
@@ -40,4 +40,18 @@ while True:
             else:
                 sala += 1
                 caminhoPv = 0
-
+                
+        while caminhoPv == 2:
+            interacaoCam +=1
+            if sala == 6:
+                print ("\nVocê está na sala: {}, contudo, a sala 6 só possui um caminho preto, que leva à sala 8.".format(sala))
+                sala = 8
+                caminhoPv = 0
+            elif sala == 8:
+                print ("\n\tNeste caminho escolhido, há um portal e você o atravessa.")
+                print ("No entanto, o portal te teletransporta aleatoriamente para uma sala de número 1 a 5\n")
+                sala = random.randint(1,5)
+                caminhoPv = 0
+            else:
+                sala += 2
+                caminhoPv = 0
